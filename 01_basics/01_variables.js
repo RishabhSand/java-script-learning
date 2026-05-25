f9a9edcdf01d0a93cf7c0c72a62c7f3284ce36e1
Rishabh Sand <rishabhsand2007@gmail.com>
2026-05-25 18:44:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345667
+let accountEmail = "rishbhsand20007"
+var accountpass = "34567"
+let accountstate="haryana"
+let accountcity;
+
+/* prefer not to use var 
+because if issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountpass,accountcity]);
