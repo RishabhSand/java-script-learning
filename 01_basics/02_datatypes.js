40975058343b77afcc7c8d63aa43286e11249238
Rishabh Sand <rishabhsand2007@gmail.com>
2026-05-25 18:44:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+console.log("Rishabh");
+let age= 19
+let gender = 1
+let loggenIn = true
+
+
+// boolean= true/false
+// null => standalone value
+// undefined => not defined
+//symbol => unique
+
+
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
