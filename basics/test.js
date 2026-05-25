f53909ea25cbc298932d6d95f6cc92239eb4e39d
Rishabh Sand <rishabhsand2007@gmail.com>
2026-05-26 00:16:56 +0530

    Delete basics directory

--- basics/test.js
+++
@@ -1,7 +0,0 @@
-const accountId = 12345667
-let accountEmail = " rishbhsand20007"
-var accountpass = "34567"
-let accountstate="haryana"
-accountcity;
-
-console.table([accountId,accountEmail,accountpass,accountcity]);
