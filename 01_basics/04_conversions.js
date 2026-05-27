e94ebdaf35b9174129b3c71f356b5d1c60203b79
Rishabh Sand <rishabhsand2007@gmail.com>
2026-05-27 07:38:10 +0000

    conversions

---
+++ 01_basics/04_conversions.js
@@ -0,0 +1,11 @@
+console.log("2"== 2);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 0);
+
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined < 0);
+
+// == can convert data type, (console.log("2"== 2); this will be true)
+// === strictly compares, different data types gives false.
