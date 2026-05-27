9a853dbef7e7f32e4aa9623fcfeb1891fd24e9b9
Rishabh Sand <rishabhsand2007@gmail.com>
2026-05-27 07:56:35 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -22,3 +22,24 @@ let loggenIn = true
 console.log(typeof null); //object
 console.log(typeof undefined);//undefined
 
+//********************************************//
+/*
+Stack ( Primitive), Heap (non-primitive)
+*/
+
+let name= "Rishabh"
+let anothername= "Mivaan"
+anothername= "Parshav"
+
+console.log(name);
+console.log(anothername);
+
+let user1 = {
+    name: "rishabh",
+    email :"rishabhsandgoglecom"
+}
+let user2 = user1
+user2.email = "google.com"
+
+console.log(user2.email);
+console.log(user1.email);
