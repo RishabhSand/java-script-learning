0448b9638fd1d9292807c1d40ae68dd9c5019f55
Rishabh Sand <rishabhsand2007@gmail.com>
2026-06-01 12:36:07 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+ const name = "Rishabh"
+ console.log(name);
+ const age = 19
+console.log(`my name is ${name} and my age is ${age}`)
+
+ const gamename = new String("RishabhGame");
+ console.log(gamename.length);
+ console.log(gamename.charAt(5));
+ console.log(gamename.indexOf("h"));
+    const newstrng = gamename.substring (0, 10) 
+
+
+    const newstring2 = "  Rishabh  "
+      console.log(newstring2);
+      console.log(newstring2.trim())
