6dd3dee73000f2acb330c3b43b8d2a2aae9462da
Rishabh Sand <rishabhsand2007@gmail.com>
2026-06-17 11:48:05 +0000

    07_dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+// let mycreatedDate = new Date(2026 , 3 , 4 , 17, 6)
+// console.log(mycreatedDate);
+// console.log(mycreatedDate.toLocaleString());
+
+// let mycreatedDate = new Date("07-17-2026")
+// console.log(mycreatedDate.toLocaleString());
+
+
+
