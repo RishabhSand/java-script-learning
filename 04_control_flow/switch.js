c8fffb7e3e1aa1de1fa538257763752dc8ac6593
Rishabh Sand <rishabhsand2007@gmail.com>
2026-06-22 11:52:54 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,43 @@
+// switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+// switch (month) {
+//     case 1:
+//          console.log("january");
+//           break;
+//     case 2:
+//          console.log("february");
+//           break;
+//     case 3:
+//          console.log("march");
+//           break;
+
+//     default:
+//         console.log("invalid month");
+//         break;
+// }
+
+const month = "march";
+switch (month) {
+      case ("jan"):
+            console.log("first month")
+            break;
+      case ("feb"):
+            console.log("second month")
+            break;
+      case ("march"):
+            console.log("third month")
+            break;
+
+      default:
+            break;
+}
